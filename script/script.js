41bb37e1450ff3e74c1b8a8eea4b59f1ea2d1821
kyujin-yamada <yamatyan.circle@gmail.com>
2020-04-08 00:08:19 +0900

    first comment

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 (() => {
+    window.isKeyDown = {};
+    
     const CANVAS_WIDTH = 640;
     const CANVAS_HEIGHT = 480;
 
@@ -7,12 +9,7 @@
     let ctx = null;
     let image = null;
     let startTime = null;
-    let viperX = CANVAS_WIDTH / 2;
-    let viperY = CANVAS_HEIGHT / 2;
-    // 自機が登場
-    let isComing = false;
-    // 登場演出を開始した際のタイムスタンプ
-    let comingStart = null;
+    let viper = null
 
     window.addEventListener('load', () => {
         util = new Canvas2DUtility(document.body.querySelector('#main_canvas'));
@@ -28,38 +25,42 @@
         });
     }, false);
 
+    /**
+     *  初期化
+     */
+
     function initialize() {
         // canvasの大きさを設定
         canvas.width = CANVAS_WIDTH;
         canvas.height = CANVAS_HEIGHT;
 
         // 登場シーンからスタートするための設定
-        isComing = true;
-        comingStart = Date.now();
-        viperY = CANVAS_HEIGHT;
+        viper = new Viper(ctx, 0, 0, 64, 64, image);
+        viper.setComing(
+            CANVAS_WIDTH / 2,
+            CANVAS_HEIGHT + 50,
+            CANVAS_WIDTH / 2,
+            CANVAS_HEIGHT - 100
+        );
     }
+    /*
+    *イベント設定
+    */
 
     function eventSetting() {
+        //　キーの押した時に呼び出されるイベント
         window.addEventListener('keydown', (event) => {
-            // 登場シーン
-            if(isComing === true){return;}
-            switch (event.key) {
-                case 'ArrowLeft':
-                    viperX -= 15;
-                    break;
-                case 'ArrowRight':
-                    viperX += 15;
-                    break;
-                case 'ArrowUp':
-                    viperY -= 15;
-                    break;
-                case 'ArrowDown':
-                    viperY += 15;
-                    break;
-            }
+            // キーの押した状態を管理
+            isKeyDown[`key_${event.key}`] = true;
+        }, false);
+        // キーが離された時に呼び出されるイベント
+        window.addEventListener('keyup', (event) =>{
+            isKeyDown[`key_${event.key}`] = false;
         }, false);
     }
-
+/*
+    描画処理
+*/
     function render() {
         // グローバルなαを必ず1.0で描画処理を開始する
         ctx.globalAlpha = 1.0;
@@ -68,26 +69,13 @@
         // 経過時間の取得
         let nowTime = (Date.now() - startTime) / 1000;
 
-        if (isComing === true){
-            // 登場シーンの経過時間
-            let justTime = Date.now();
-            let comingTime = (justTime - comingStart) / 1000;
-            viperY = CANVAS_HEIGHT - comingTime * 50;
-            if(viperY <= CANVAS_HEIGHT - 100){
-                isComing = false;
-                viperY = CANVAS_HEIGHT - 100;
-            }
-            // justTimeを100で割った時余りが50より小さくなる場合半透明
-            if(justTime % 100 < 50){
-                ctx.globalAlpha = 0.1;
-            }
-        }
-        ctx.drawImage(image, viperX, viperY)
+        viper.update();
+
         requestAnimationFrame(render);
     }
+    /* 特定のは二のランダム */
     function generateRandomInt(range) {
         let random = Math.random();
         return Math.floor(random * range);
     }
-
 })();
